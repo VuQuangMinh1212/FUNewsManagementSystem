b5270426c76544e7c53de3eedcdbc2b933f4ffaa
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-02-13 15:29:15 +0700

    Add project files.

---
+++ FUNewsManagementSystem/Controllers/SystemAccountsController.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FUNewsManagementSystem.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace FUNewsManagementSystem.Controllers
+{
+    public class SystemAccountsController : Controller
+    {
+        private readonly FunewsManagementContext _context;
+
+        public SystemAccountsController(FunewsManagementContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SystemAccounts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.SystemAccounts.ToListAsync());
+        }
+
+        // GET: SystemAccounts/Details/5
+        public async Task<IActionResult> Details(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var systemAccount = await _context.SystemAccounts
+                .FirstOrDefaultAsync(m => m.AccountId == id);
+            if (systemAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(systemAccount);
+        }
+
+        // GET: SystemAccounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SystemAccounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("AccountId,AccountName,AccountEmail,AccountRole,AccountPassword")] SystemAccount systemAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(systemAccount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(systemAccount);
+        }
+
+        // GET: SystemAccounts/Edit/5
+        public async Task<IActionResult> Edit(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var systemAccount = await _context.SystemAccounts.FindAsync(id);
+            if (systemAccount == null)
+            {
+                return NotFound();
+            }
+            return View(systemAccount);
+        }
+
+        // POST: SystemAccounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(short id, [Bind("AccountId,AccountName,AccountEmail,AccountRole,AccountPassword")] SystemAccount systemAccount)
+        {
+            if (id != systemAccount.AccountId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(systemAccount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SystemAccountExists(systemAccount.AccountId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(systemAccount);
+        }
+
+        // GET: SystemAccounts/Delete/5
+        public async Task<IActionResult> Delete(short? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var systemAccount = await _context.SystemAccounts
+                .FirstOrDefaultAsync(m => m.AccountId == id);
+            if (systemAccount == null)
+            {
+                return NotFound();
+            }
+
+            return View(systemAccount);
+        }
+
+        // POST: SystemAccounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(short id)
+        {
+            var systemAccount = await _context.SystemAccounts.FindAsync(id);
+            if (systemAccount != null)
+            {
+                _context.SystemAccounts.Remove(systemAccount);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SystemAccountExists(short id)
+        {
+            return _context.SystemAccounts.Any(e => e.AccountId == id);
+        }
+
+        [HttpGet] 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            // Check the database for a matching user
+            var user = await _context.SystemAccounts
+                .FirstOrDefaultAsync(u => u.AccountEmail == email && u.AccountPassword == password);
+
+            if (user != null)
+            {
+                string roleName = user.AccountRole == 1 ? "Staff" : "Lecturer";
+                // Create the user claims
+                var claims = new List<Claim>
+            {
+
+                new Claim(ClaimTypes.Name, user.AccountName),
+                new Claim(ClaimTypes.Role, roleName )
+            };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = true
+                };
+
+                // Sign in the user
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    authProperties);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Invalid credentials, show error message
+            ViewBag.Error = "Invalid username or password";
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ FUNewsManagementSystem/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using FUNewsManagementSystem.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<FunewsManagementContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/SystemAccounts/Login";
+        options.LogoutPath = "/SystemAccounts/Logout";
+        options.AccessDeniedPath = "/SystemAccounts/AccessDenied";
+    });
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(60);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseAuthentication();
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=SystemAccounts}/{action=Login}/{id?}");
+
+app.Run();
