f184d5ea05d81a87cb1cb67ffe637473006d72b6
Vinh0209 <vanvinhhh2903@gmail.com>
2025-02-16 19:00:23 +0700

    layout admin

---
+++ FUNewsManagementSystem/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FUNewsManagementSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult ManageAccounts()
+        {
+            return View();
+        }
+        public IActionResult Report()
+        {
+            return View();
+        }
+        public IActionResult EditAccount()
+        {
+            return View();
+        }
+    }
+}
