36f4261529b17660106ed341abac352077a9fe30
KietPHG <phamkiet2234@gmail.com>
2025-02-17 01:38:54 +0700

    Category Manage

--- FUNewsManagementSystem/Controllers/StaffController.cs
+++ FUNewsManagementSystem/Controllers/StaffController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FUNewsManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace FUNewsManagementSystem.Controllers
 {
     public class StaffController : Controller
     {
+        private readonly FunewsManagementContext _context;
+
+        public StaffController(FunewsManagementContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult ManageCategories()
         {
-            return View();
+            List<Category> categories = new List<Category>();
+            categories = _context.Categories.AsEnumerable().ToList();
+            return View(categories);
+        }
+
+        public IActionResult AddCategory([FromBody] Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+
+            var categories = _context.Categories.AsEnumerable().ToList();
+
+            return PartialView("_CategoryList", categories);
         }
 
         public IActionResult ManageNews()
