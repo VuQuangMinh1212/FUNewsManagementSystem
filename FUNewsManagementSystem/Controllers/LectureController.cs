01e94a79cf058814e9cdbd2c595d5b761051b7bb
Vinh0209 <vanvinhhh2903@gmail.com>
2025-02-16 20:54:04 +0700

    layout lecture

---
+++ FUNewsManagementSystem/Controllers/LectureController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FUNewsManagementSystem.Controllers
+{
+    public class LectureController : Controller
+    {
+
+        public IActionResult ViewNewsArticles()
+        {
+            return View();
+        }
+    }
+}
